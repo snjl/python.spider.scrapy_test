3e03bc76454489b589b39dc7ef6876a4b64959ec
sn349245467 <349245467@qq.com>
2019-01-28 18:20:33 +0800

    提交

---
+++ pipelines.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import pymongo
+from scrapy.exceptions import DropItem
+
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class TutorialPipeline(object):
+    def __init__(self):
+        self.limit = 50
+
+    def process_item(self, item, spider):
+        if item['text']:
+            if len(item['text']) > self.limit:
+                item['text'] = item['text'][0:self.limit].rstrip() + '...'
+            return item
+        else:
+            print('text is null')
+            return DropItem("Missing Text")
+
+# MongoDB存储数据管道
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    # 从settings中拿到配置
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
+
+    # 爬虫启动时需要进行的操作，初始化MongoDB对象
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    # 最重要的process_item
+    def process_item(self, item, spider):
+        # 使用这样的name比较灵活
+        name = item.__class__.__name__
+        self.db[name].insert(dict(item))
+        return item
+
+    # 管道完毕时自动运行
+    def close_spider(self, spider):
+        self.client.close()
---
+++ spiders/QuotesSpider.py
@@ -0,0 +1,34 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+from tutorial.items import TutorialItem
+
+
+class QuotesspiderSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        bs_obj = BeautifulSoup(response.text)
+        items = bs_obj.find_all('div', {'class': 'quote'})  # 获取列表
+        for item in items:
+            item_field = TutorialItem()  # 每一个item信息存储到item_field里
+            text = item.find('span', {'itemprop': 'text', 'class': 'text'}).text
+            author = item.find('small', {'class': 'author'}).text
+            tags = item.find_all('a', {'class': 'tag'})
+            tags = [tag.text for tag in tags]  # 获取tags列表里的每一个tag的文本
+            item_field['text'] = text  # 存储数据到item_field
+            item_field['author'] = author
+            item_field['tags'] = tags
+            self.logger.info(item_field['text'])
+            yield item_field  # 使用生成器，每次调用都会从结束处开始，会生成新的item_field，爬取和计算后会返回数据
+
+        # 获取下一页，由于使用BeautifulSoup比较麻烦，而且错误处理比较不方便，所以使用css选择器
+        next_page = response.css('.pager .next a::attr(href)').extract_first()
+        # 如果next_page存在
+        # if next_page:
+        #     # 使用urljoin获取绝对地址
+        #     next_url = response.urljoin(next_page)
+        #     # 回调函数，继续调用该parse函数，传入next_url进行请求
+        #     yield scrapy.Request(url=next_url, callback=self.parse)
